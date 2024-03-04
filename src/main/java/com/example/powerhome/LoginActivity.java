2e15fa02619cbc31cd60156bc7d437872b078a64
暁Olivier <128840434+OlvOpinel@users.noreply.github.com>
2024-03-04 11:07:44 +0100

    SRC2
    Src avec la BD qui fonctionne avec mon localhost

--- src/main/java/com/example/powerhome/LoginActivity.java
+++ src/main/java/com/example/powerhome/LoginActivity.java
@@ -7,9 +7,20 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.google.android.material.textfield.TextInputEditText;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class LoginActivity extends AppCompatActivity {
@@ -19,8 +30,11 @@ public class LoginActivity extends AppCompatActivity {
     TextInputEditText textInputEditTextName, textInputEditTextPassword;
     private Button connectBtn;
 
-    private String Name;
-    private String Password;
+    private String username;
+    private String password;
+
+
+    private Database database;
 
 
     @Override
@@ -29,25 +43,74 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         errorConnectAccountTextView = findViewById(R.id.errorConnectAccountTextView);
-        textInputEditTextName = findViewById(R.id.etName);
-        textInputEditTextPassword = findViewById(R.id.etPassword);
+        textInputEditTextName = findViewById(R.id.username);
+        textInputEditTextPassword = findViewById(R.id.password);
         connectBtn = findViewById(R.id.connectBtn);
+
+        database = new Database(getApplicationContext());
 
         connectBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Name = textInputEditTextName.getText().toString();
-                Password = textInputEditTextPassword.getText().toString();
+                username = textInputEditTextName.getText().toString();
+                password = textInputEditTextPassword.getText().toString();
 
+                connectuser();
                 //LANCEER LA REQUÊTE POUR CONNECTER LE USER
             }
         });
     }
 
 
+    public void onApiResponse(JSONObject response) {
+        Boolean success = null;
+        String error = "";
+
+        try {
+            success = response.getBoolean("success");
+
+            if (success == true) {
+                Intent menuactivity = new Intent(getApplicationContext(), MenuActivity.class);
+                menuactivity.putExtra("username", username);
+                startActivity(menuactivity);
+                finish();
+            } else {
+                error = response.getString("error");
+                errorConnectAccountTextView.setVisibility(View.VISIBLE);
+                errorConnectAccountTextView.setText(error);
+            }
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+            Toast.makeText(getApplicationContext(), e.toString(), Toast.LENGTH_LONG).show();
+        }
+    }
+
+
     public void connectuser() {
         //10.0.2.2 LOCALHOST DANS GOOGLE
-        String URL = "http://localhost:8888/powerhome/";
+        String url = "http://10.0.2.2/powerhome3/actions/connectUser.php";
+
+        Map<String, String> params = new HashMap<>();
+        params.put("username", username);
+        params.put("password", password);
+        JSONObject parameters = new JSONObject(params);
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, url, parameters, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                onApiResponse(response);
+                Toast.makeText(getApplicationContext(), "OPERATION SUCCESSFUL", Toast.LENGTH_LONG).show();
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(), error.getMessage().toString(), Toast.LENGTH_LONG).show();
+            }
+        });
+
+        database.queue.add(jsonObjectRequest);
+
     }
 
 
--- src/main/java/com/example/powerhome/RegisterActivity.java
+++ src/main/java/com/example/powerhome/RegisterActivity.java
@@ -8,8 +8,19 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.google.android.material.textfield.TextInputEditText;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -17,32 +28,90 @@ public class RegisterActivity extends AppCompatActivity {
     TextInputEditText textInputEditTextName, textInputEditTextEmail, textInputEditTextPassword;
     private Button subBtn;
 
-    private String Name;
-    private String Email;
-    private String Password;
+    private String username;
+    private String email;
+    private String password;
+
+    private Database database;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        textInputEditTextName = findViewById(R.id.etName);
-        textInputEditTextEmail = findViewById(R.id.etMail);
-        textInputEditTextPassword = findViewById(R.id.etPassword);
+        textInputEditTextName = findViewById(R.id.Username);
+        textInputEditTextEmail = findViewById(R.id.Email);
+        textInputEditTextPassword = findViewById(R.id.Password);
+        errorCreateAccountTextView = findViewById(R.id.errorCreateAccountTextView);
         subBtn = findViewById(R.id.subBtn);
+
+        database = new Database(getApplicationContext());
 
         subBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Name = textInputEditTextName.getText().toString();
-                Email = textInputEditTextEmail.getText().toString();
-                Password = textInputEditTextPassword.getText().toString();
+                username = textInputEditTextName.getText().toString();
+                email = textInputEditTextEmail.getText().toString();
+                password = textInputEditTextPassword.getText().toString();
 
+                createAccount();
                 //LANCER LA REQUÊTE POUR INSCRIRE LE USER
             }
         });
-
     }
+
+    public void onApiResponse(JSONObject response) {
+        Boolean success = null;
+        String error = "";
+
+        try {
+            success = response.getBoolean("success");
+
+            if (success == true) {
+                Intent menuactivity = new Intent(getApplicationContext(), MenuActivity.class);
+                menuactivity.putExtra("username", username);
+                startActivity(menuactivity);
+                finish();
+            } else {
+                error = response.getString("error");
+                errorCreateAccountTextView.setVisibility(View.VISIBLE);
+                errorCreateAccountTextView.setText(error);
+            }
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+            Toast.makeText(getApplicationContext(), e.toString(), Toast.LENGTH_LONG).show();
+        }
+    }
+
+
+    public void createAccount () {
+        //10.0.2.2 LOCALHOST DANS GOOGLE
+        String url = "http://10.0.2.2/powerhome3/actions/createAccount.php";
+
+        Map<String, String> params = new HashMap<>();
+        params.put("username", username);
+        params.put("email", email);
+        params.put("password", password);
+
+        JSONObject parameters = new JSONObject(params);
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, url, parameters, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                onApiResponse(response);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(), error.getMessage().toString(), Toast.LENGTH_LONG).show();
+
+            }
+        });
+
+        database.queue.add(jsonObjectRequest);
+    }
+
 
     public void login (View view) {
         startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
